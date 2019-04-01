d08a37c0e6924de44582f1ff003ec54565b58409
Sarah Kirk <sarah.kirk@urbanairship.com>
2019-04-01 11:31:39 -0700

    scaffolding models and schema relationships

--- haute_hiker/urls.py
+++ haute_hiker/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('reviews/', include('ciphers.urls')),
+    path('reviews/', include('reviews.urls')),
     path('admin/', admin.site.urls),
 ]
--- reviews/models.py
+++ reviews/models.py
@@ -4,3 +4,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    image_url = models.CharField(max_length=500)
+    description = models.CharField(max_length=500)
+    tags = models.ManyToManyField(Tag)
+    resemblance_to_package = models.IntegerField()
+    hot_sauce_needed = models.IntegerField()
+    texture = models.IntegerField()
+    deliciousness_level = models.IntegerField()
+    created = models.DateTimeField('date created')
+    updated = models.DateTimeField('date updated')
+
+
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=20)
+    created = models.DateTimeField('date created')
+    updated = models.DateTimeField('date updated')
