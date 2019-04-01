8ecaf263b929a39ab892acf10a4fdae0a3f48581
Sarah Kirk <sarah.kirk@urbanairship.com>
2019-04-01 11:36:57 -0700

    adds migrations to reviews app

--- reviews/models.py
+++ reviews/models.py
@@ -4,6 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=20)
+    created = models.DateTimeField('date created')
+    updated = models.DateTimeField('date updated')
+    
 class Review(models.Model):
     image_url = models.CharField(max_length=500)
     description = models.CharField(max_length=500)
@@ -12,11 +17,5 @@ class Review(models.Model):
     hot_sauce_needed = models.IntegerField()
     texture = models.IntegerField()
     deliciousness_level = models.IntegerField()
-    created = models.DateTimeField('date created')
-    updated = models.DateTimeField('date updated')
-
-
-class Tag(models.Model):
-    tag_name = models.CharField(max_length=20)
     created = models.DateTimeField('date created')
     updated = models.DateTimeField('date updated')
