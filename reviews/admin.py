2308b12d6ed6520c5e21cad8a243c557272e7888
Sarah Kirk <sarah.kirk@urbanairship.com>
2019-06-03 11:42:14 -0700

    add image to admin functionality

--- reviews/admin.py
+++ reviews/admin.py
@@ -4,8 +4,9 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from .models import Review, Tag, Article
+from .models import Review, Tag, Article, Image
 
 admin.site.register(Review)
 admin.site.register(Tag)
 admin.site.register(Article)
+admin.site.register(Image)
